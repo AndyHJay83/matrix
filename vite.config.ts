6dcfd529b1c80672a09789762be973ef73f4eeea
AndyHJay83 <120287095+AndyHJay83@users.noreply.github.com>
2025-06-18 11:58:29 +0100

    fox

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/matrix/',
   plugins: [
     react(),
     VitePWA({
@@ -17,8 +18,8 @@ export default defineConfig({
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/matrix/',
+        start_url: '/matrix/',
         icons: [
           {
             src: 'pwa-192x192.png',
